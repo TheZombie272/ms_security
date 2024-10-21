e04bb5b7ee4b24b7e085cc929279f0f8963e50b8
tbotina <tania.botina22736@ucaldas.edu.co>
2024-10-21 00:46:18 -0500

    Getter y Setter para todos los modelos

--- src/main/java/com/danielflores38153/ms_security/Models/Role.java
+++ src/main/java/com/danielflores38153/ms_security/Models/Role.java
@@ -20,4 +20,28 @@ public class Role {
         this.name = name;
         this.description = description;
     }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
