022d4df58d02465a88e74894f2bd4a52e43263ef
TheZombie272 <uzumakyinuyasha@gmail.com>
2025-11-09 14:54:24 -0500

    forma sin 2fa

--- src/main/java/com/danielflores38153/ms_security/Controllers/SecurityController.java
+++ src/main/java/com/danielflores38153/ms_security/Controllers/SecurityController.java
@@ -48,55 +48,32 @@ public class SecurityController {
             User theActualUser = this.theUserRepository.getUserByEmail(theNewUser.getEmail());
             if (theActualUser != null
                     && theActualUser.getPassword().equals(theEncryptionService.convertSHA256(theNewUser.getPassword()))) {
-                String token2FA = PasswordService.generateToken();
-    
+
+                // Limpiar password antes de devolver
                 theActualUser.setPassword("");
-                theResponse.put("user", theActualUser);
-    
-                //crear sesion
+
+                // Generar token JWT inmediatamente (sin 2FA)
+                String token = this.theJwtService.generateToken(theActualUser);
+
+                // crear sesion y guardar el token JWT en la sesión
                 Session theUserSession = new Session();
-                theUserSession.setUser(theActualUser); // Establece el usuario para la sesión
-                theUserSession.setToken2FA(token2FA); // Guarda el token 2FA
+                theUserSession.setUser(theActualUser);
+                theUserSession.setToken(token);
                 theUserSession.setType("session");
-                Session theSendSession=theSessionRepository.save(theUserSession); // Guarda la sesión en la base de datos
-    
-                // Enviar token 2FA por correo 
-                /*this.theEmailService.sendEmail(theActualUser.getEmail(), "Código",
-                        "Sus datos: " + theUserSession.get_id() + " Token: " + token2FA); */
-                  System.out.println(token2FA);
-                theSendSession.setToken2FA(null);
+                Session theSendSession = theSessionRepository.save(theUserSession);
+
+                // No devolvemos el token dentro del objeto session por seguridad
+                theSendSession.setToken(null);
+
+                theResponse.put("user", theActualUser);
+                theResponse.put("token", token);
                 theResponse.put("session", theSendSession);
                 return theResponse;
             } else {
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
                 return theResponse;
             }
-    
-        }
-    
-        // Verificar token y generar jwt
-        @PostMapping("/verify2fa")
-        public HashMap<String, Object> verify2FA(@RequestBody Session theSession, final HttpServletResponse response) throws IOException {
-            HashMap<String, Object> theResponse = new HashMap<>();
-            Session theUserSession = this.theSessionRepository.findById(theSession.get_id()).orElse(null);
-            if (theUserSession != null && theUserSession.getToken2FA().equals(theSession.getToken2FA())
-            && !theUserSession.getUsed() && theUserSession.getType().equals("session")) {
-                // Generar token JWT
-                String token = this.theJwtService.generateToken(theUserSession.getUser());
-    
-                // Actualizar la sesión
-                theUserSession.setToken(token);
-                theUserSession.setUsed(true);
-                theSessionRepository.save(theUserSession);
 
-                theResponse.put("token", token);
-                theResponse.put("user", theUserSession.getUser());
-    
-                return theResponse;
-            }  else {
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Código de verificación incorrecto");
-                return null;
-            }
         }
     
         @PostMapping("/reset_password")
